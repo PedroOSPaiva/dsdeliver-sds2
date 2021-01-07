d2c313f065f3fac034d166ee37e98cb8f3ba86d3
unknown <pedrohenriqueospaiva@gmail.com>
2021-01-06 23:11:48 -0300

    GET /products

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/dto/ProductDTO.java
@@ -0,0 +1,81 @@
+package com.devsuperior.dsdeliver.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsdeliver.entities.Product;
+
+public class ProductDTO implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private Double price;
+	private String drescription;
+	private String imageUri;
+	
+	public ProductDTO() {
+	}
+
+	public ProductDTO(Long id, String name, Double price, String drescription, String imageUri) {
+		
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.drescription = drescription;
+		this.imageUri = imageUri;
+	}
+	
+
+	public ProductDTO(Product entity) {
+		
+		id = entity.getId();
+		name = entity.getName();
+		price = entity.getPrice();
+		drescription = entity.getDescription();
+		imageUri = entity.getImageUri();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getDrescription() {
+		return drescription;
+	}
+
+	public void setDrescription(String drescription) {
+		this.drescription = drescription;
+	}
+
+	public String getImageUri() {
+		return imageUri;
+	}
+
+	public void setImageUri(String imageUri) {
+		this.imageUri = imageUri;
+	}
+	
+	
+}
